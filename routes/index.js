f0b889199e4799f8baad889ccacd32fc16461dcd
Luke Todd <luketodd@zoho.com>
2016-08-06 17:48:32 +0100

    Set up most of work page for mobile

--- routes/index.js
+++ routes/index.js
@@ -4,20 +4,22 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
-      title: 'BLAH BLAH',
-      data: {
+      title: 'LUKE TODD',
+      articles: {
           articleOne: {
-              title: 'Article Temp',
+              title: 'Les Paul Review',
+              image: 'http://www.fillmurray.com/g/260/100',
               content: 'Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet.',
               created: '17th July - 2016'
           },
           articleTwo: {
-              title: 'Article Temp',
+              title: 'Lombardis Restaurant Sucks',
               content: 'Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet.',
               created: '17th July - 2016'
           },
           articleThree: {
-              title: 'Article Temp',
+              title: 'The future is here - Sprockets',
+              image: 'http://www.fillmurray.com/260/100',
               content: 'Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet.',
               created: '17th July - 2016'
           }
@@ -25,8 +27,27 @@ router.get('/', function(req, res, next) {
    });
 });
 
-router.get('/about', function(req, res, next) {
-  res.render('about');
+router.get('/work', function(req, res, next) {
+  res.render('work', {
+      title: 'LUKE TODD',
+      work: {
+          ashadegreener: {
+              title: 'A Shade Greener',
+              url: 'https://www.ashadegreener.co.uk',
+              screengrab: "http://www.fillmurray.com/260/220",
+          },
+          getgovgrants: {
+              title: 'Free Boiler Grants',
+              url: 'https://www.getgovgrants.org.uk',
+              screengrab: 'http://www.fillmurray.com/260/220'
+          },
+          quickquotes: {
+              title: 'Quick Quotes Concept',
+              url: '',
+              screengrab: 'http://www.fillmurray.com/260/220'
+          }
+      }
+  });
 });
 
 router.get('/contact', function(req, res, next) {
