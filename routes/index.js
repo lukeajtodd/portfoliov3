c25886d8398c30a20d39416601024a83a0bd825c
Luke Todd <luketodd@zoho.com>
2016-08-08 10:45:15 +0100

    basic set up for desktop

--- routes/index.js
+++ routes/index.js
@@ -23,6 +23,23 @@ router.get('/', function(req, res, next) {
               content: 'Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet.',
               created: '17th July - 2016'
           }
+      },
+      work: {
+          ashadegreener: {
+              title: 'A Shade Greener',
+              url: 'https://www.ashadegreener.co.uk',
+              screengrab: "http://www.fillmurray.com/260/220",
+          },
+          getgovgrants: {
+              title: 'Free Boiler Grants',
+              url: 'https://www.getgovgrants.org.uk',
+              screengrab: 'http://www.fillmurray.com/260/220'
+          },
+          quickquotes: {
+              title: 'Quick Quotes Concept',
+              url: '',
+              screengrab: 'http://www.fillmurray.com/260/220'
+          }
       }
    });
 });
