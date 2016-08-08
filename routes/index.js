42fa0829d0ff5f315143760631c242ec2b2b2a95
Luke Todd <luketodd@zoho.com>
2016-08-08 16:23:14 +0100

    Continued work on the desktop look and feel

--- routes/index.js
+++ routes/index.js
@@ -28,17 +28,23 @@ router.get('/', function(req, res, next) {
           ashadegreener: {
               title: 'A Shade Greener',
               url: 'https://www.ashadegreener.co.uk',
-              screengrab: "http://www.fillmurray.com/260/220",
+              description: 'A money saving and deals website. I personally worked on the forms and multiple aspects of the UI.',
+              screengrab: '/images/ashadegreener.png',
+              mobile_screengrab: '/images/ashadegreener-mobile.png'
           },
           getgovgrants: {
               title: 'Free Boiler Grants',
               url: 'https://www.getgovgrants.org.uk',
-              screengrab: 'http://www.fillmurray.com/260/220'
+              description: 'A website for free boiler grants. Front end custom built using the Foundation framework and Laravel back-end.',
+              screengrab: '/images/getgovgrants.png',
+              mobile_screengrab: '/images/getgovgrants-mobile.png'
           },
           quickquotes: {
               title: 'Quick Quotes Concept',
               url: '',
-              screengrab: 'http://www.fillmurray.com/260/220'
+              description: 'This was the beginning of a site for window quotes. The app uses javascript and some jQuery sortable drag and drop effects.',
+              screengrab: '/images/quickquotes.png',
+              mobile_screengrab: ''
           }
       }
    });
