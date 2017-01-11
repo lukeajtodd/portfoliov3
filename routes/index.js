decc0f697af4bd688b8197d4457b4f8badf00ad8
Luke Todd <luke.todd@plus.net>
2017-01-11 13:41:47 +0000

    Now generates a random colour set for the logo on page load

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,28 @@ router.get('/', function(req, res, next) {
               description: 'A React & Firebase tool to pick and save colour palettes.',
               screengrab: 'https://www.fillmurray.com/700/240'
           }
-      }
+      },
+      colours: [
+        "#F44336",
+        "#E91E63",
+        "#9C27B0",
+        "#673AB7",
+        "#3F51B5",
+        "#2196F3",
+        "#03A9F4",
+        "#00BCD4",
+        "#009688",
+        "#4CAF50",
+        "#8BC34A",
+        "#CDDC39",
+        "#FFEB3B",
+        "#FFC107",
+        "#FF9800",
+        "#FF5722",
+        "#795548",
+        "#9E9E9E",
+        "#607D8B"
+      ]
    });
 });
 
